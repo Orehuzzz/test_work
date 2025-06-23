e8703f89424cff09faea55c3b5802e397a9037cc
Orehuzzz <oreshin.vitalik@gmail.com>
2025-06-23 15:00:36 +0300

    this is the end

--- csv_result/take_result.py
+++ csv_result/take_result.py
@@ -5,5 +5,6 @@ data_df = pd.read_csv('data.csv')
 
 combined_df = pd.concat([regions_df, data_df], axis=1)
 
+#Объединяем два файла в один
 combined_df.to_csv('result.csv', index=False, encoding='utf-8-sig')
 print("Объединённый файл сохранён как 'result.csv'")
